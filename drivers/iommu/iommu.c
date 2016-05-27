e818a4f74afc08693ade849cfff50bf7fa445e51
Janghyuck Kim <janghyuck.kim@samsung.com>
2016-05-27 10:14:35 +0900

    [COMMON] iommu: fix detach_device() condition
    attach_device() was fixed not to care group condition. So
    detach_device() should be changed as same condition.
    
    Change-Id: I300cf0927588dba4d79502e7a5beb07b479396fd
    Signed-off-by: Janghyuck Kim <janghyuck.kim@samsung.com>

--- drivers/iommu/iommu.c
+++ drivers/iommu/iommu.c
@@ -1398,6 +1398,9 @@ void iommu_detach_device(struct iommu_domain *domain, struct device *dev)
 {
 	struct iommu_group *group;
 
+	/* HACK: We don't care iommu group */
+	return __iommu_detach_device(domain, dev);
+
 	group = iommu_group_get(dev);
 	if (!group)
 		return;
