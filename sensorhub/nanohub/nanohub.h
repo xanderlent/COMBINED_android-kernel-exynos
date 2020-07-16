a10bba7c6fce33cd84d764f11db70eef9e7f0af4
Ben Fennema <fennema@google.com>
2020-07-15 19:39:10 -0700

    Add nanohub kernel driver module
    Bug: 161410878
    Signed-off-by: Ben Fennema <fennema@google.com>
    Change-Id: I06292c26682e2ab1fd9c0e3d352eef5a4a1f1c0d

---
+++ sensorhub/nanohub/nanohub.h
@@ -0,0 +1,25 @@
+#ifndef __NANOHUB_NANOHUB_H
+#define __NANOHUB_NANOHUB_H
+#include <linux/types.h>
+struct nanohub_flash_bank {
+	int bank;
+	u32 address;
+	size_t length;
+};
+struct nanohub_platform_data {
+	u32 wakeup_gpio;
+	u32 nreset_gpio;
+	u32 boot0_gpio;
+	u32 irq1_gpio;
+	u32 irq2_gpio;
+	u32 spi_cs_gpio;
+#ifdef CONFIG_NANOHUB_BL
+	u32 bl_max_speed_hz;
+	u32 bl_addr;
+	u32 num_flash_banks;
+	struct nanohub_flash_bank *flash_banks;
+	u32 num_shared_flash_banks;
+	struct nanohub_flash_bank *shared_flash_banks;
+#endif
+};
+#endif /* __NANOHUB_NANOHUB_H */
