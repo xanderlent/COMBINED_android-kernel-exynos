7defc7b5fea591f173da8ac5fe4adce8c8722d7a
Jiyoung Jeong <ji_0.jeong@samsung.com>
2020-11-10 14:06:50 +0900

    [COMMON] soc: samsung: cpif: support kernel 4.19
    Change-Id: I46d0db77e8a7a1486e356fe1c56a622cd41ed90d
    Signed-off-by: Jiyoung Jeong <ji_0.jeong@samsung.com>

--- include/linux/netdevice.h
+++ include/linux/netdevice.h
@@ -3035,6 +3035,9 @@ struct softnet_data {
 	unsigned int		dropped;
 	struct sk_buff_head	input_pkt_queue;
 	struct napi_struct	backlog;
+#ifdef CONFIG_MODEM_IF_NET_GRO
+	struct napi_struct	*current_napi;
+#endif
 
 };
 
@@ -3627,6 +3630,10 @@ gro_result_t napi_gro_frags(struct napi_struct *napi);
 struct packet_offload *gro_find_receive_by_type(__be16 type);
 struct packet_offload *gro_find_complete_by_type(__be16 type);
 extern struct napi_struct *get_current_napi_context(void);
+
+#if defined(CONFIG_EXYNOS_MODEM_IF)
+struct napi_struct *napi_get_current(void);
+#endif
 
 static inline void napi_free_frags(struct napi_struct *napi)
 {
