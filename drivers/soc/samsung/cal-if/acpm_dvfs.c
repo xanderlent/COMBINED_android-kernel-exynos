1a647101e92e80afc18f183b87b9b6e9d3066010
mullerf <mullerf@google.com>
2021-04-07 10:48:34 -0700

    Re-enable pm_qos_request
    pm_qos_request was disabled in 'commit eee7a2e81110 ("[HACK] drivers:
    soc: disable pm_qos_request and ect parse")' along with ect parsing.
    
    ect parsing was then re-introduced in 'commit b36669c0993d ("[common]
    soc: samsung: cal-if: enable ect initilaztion")', but not pm_qos_request.
    
    In the radio.img starting at version g307rc-81754-210331-B-7248187 and
    later, a call to acpm_noti_mif_callback is done. This is leading to a
    call on a null mif_request_from_acpm, leading to a crash of the kernel.
    
    Re-enabling pm_qos_request to fix this issue.
    
    Test: irq/97-11900000.acpm_ipc settles and isn't maxing the CPU usage
    anymore. Also, no more kernel crash related to this.
    
    Bug: 184293893
    
    Signed-off-by: mullerf <mullerf@google.com>
    Change-Id: I0a4f0d8b07a8edc0161b851efc4dc12df9fe1d78

--- drivers/soc/samsung/cal-if/acpm_dvfs.c
+++ drivers/soc/samsung/cal-if/acpm_dvfs.c
@@ -125,6 +125,7 @@ int exynos_acpm_set_volt_margin(unsigned int id, int volt)
 
 	return ret;
 }
+
 #if 0
 #if defined(CONFIG_EXYNOS_THERMAL) && !defined(CONFIG_EXYNOS_ACPM_THERMAL)
 int exynos_acpm_set_cold_temp(unsigned int id, bool is_cold_temp)
@@ -154,11 +155,13 @@ int exynos_acpm_set_cold_temp(unsigned int id, bool is_cold_temp)
 }
 #endif
 #endif
+
 static void acpm_noti_mif_callback(unsigned int *cmd, unsigned int size)
 {
 	pr_info("%s : req %d KHz\n", __func__, cmd[1]);
 	pm_qos_update_request(&mif_request_from_acpm, cmd[1]);
 }
+
 #if 0
 #if defined(CONFIG_EXYNOS_THERMAL) && !defined(CONFIG_EXYNOS_ACPM_THERMAL)
 static int acpm_cpu_tmu_notifier(struct notifier_block *nb, unsigned long event, void *v)
@@ -260,6 +263,7 @@ static void acpm_dvfs_get_gpu_cold_temp_list(struct device *dev)
 }
 #endif
 #endif
+
 static int acpm_dvfs_probe(struct platform_device *pdev)
 {
 	struct device *dev = &pdev->dev;
@@ -275,7 +279,7 @@ static int acpm_dvfs_probe(struct platform_device *pdev)
 	if (ret < 0)
 		return ret;
 
-	//pm_qos_add_request(&mif_request_from_acpm, PM_QOS_BUS_THROUGHPUT, 0);
+	pm_qos_add_request(&mif_request_from_acpm, PM_QOS_BUS_THROUGHPUT, 0);
 
 #if defined(CONFIG_EXYNOS_THERMAL) && !defined(CONFIG_EXYNOS_ACPM_THERMAL)
 //	acpm_dvfs_get_cpu_cold_temp_list(dev);
