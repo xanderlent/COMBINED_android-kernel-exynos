0f7dc78f90e19c1df22864e9fde7c1c34804b9a8
Chuong Hoang <chuongh@google.com>
2021-11-17 19:33:50 +0000

    Ignore packet errors during exit-doze brightness read
    MIPI lines go low during MCU handoff could cause the error but the
    data is still correctly sent. So let's just ignore the return code and
    copy the data over if it's not the default zero value.
    
    Also use MIPI_DCS_GET_DISPLAY_BRIGHTNESS (0x52) instead of 0x51 for
    consistency purposes.
    
    Bug: 206605823
    Test: Manual
    Signed-off-by: Chuong Hoang <chuongh@google.com>
    Change-Id: Ieeaaf2b99671906d1e0dca1e4126eea5e2a5b15b

--- drivers/video/fbdev/exynos/dpu30/panels/panel-boe-wf012fbm.c
+++ drivers/video/fbdev/exynos/dpu30/panels/panel-boe-wf012fbm.c
@@ -347,8 +347,7 @@ static int wf012fbm_exit_hbm(struct exynos_panel_device *panel)
 static int wf012fbm_exit_doze(struct exynos_panel_device *panel)
 {
 	struct dsim_device *dsim = get_dsim_drvdata(0);
-	int ret;
-	unsigned char buf[1];
+	unsigned char buf[1] = {0};
 
 	DPU_INFO_PANEL("%s +\n", __func__);
 	mutex_lock(&panel->ops_lock);
@@ -358,10 +357,9 @@ static int wf012fbm_exit_doze(struct exynos_panel_device *panel)
 	dsim_write_data_seq(dsim, false, MIPI_DCS_EXIT_IDLE_MODE);
 
 	/* Read brightness */
-	ret = dsim_read_data(dsim, MIPI_DSI_DCS_READ, 0x51, sizeof(buf), buf);
-	if (ret < 0) {
-		dsim_err("Failed to read brightness reg from panel\n");
-	} else {
+	dsim_read_data(dsim, MIPI_DSI_DCS_READ,
+			MIPI_DCS_GET_DISPLAY_BRIGHTNESS, sizeof(buf), buf);
+	if (buf[0] != 0) {
 		panel->bl->props.brightness = buf[0];
 	}
 	mutex_unlock(&panel->ops_lock);
